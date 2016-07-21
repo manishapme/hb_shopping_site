180a5db3d38725b48f8fa1746264ea69dc25769a
Hackbright Student <info@hackbrightacademy.com>
2016-07-21 13:06:37 -0700

    Created dictionary for cart

--- shoppingsite.py
+++ shoppingsite.py
@@ -71,13 +71,27 @@ def shopping_cart():
     # - hand to the template the total order cost and the list of melon types
     melon_list = session['cart_items']
     melon_count_dict = {}
-    for melon in melon_list:
-        melon_count_dict[melon] = melon_count_dict.get(melon, 0) + 1
+    order_total = 0
+    for melon_id in melon_list:
+        # grabbing the melon object based on id stored in session
+        melon = melons.get_by_id(melon_id)
+        # creating an entry in the dictionary for each melon, with id as key
+        # and storing another dictionary of attributes for that melon
+        # setdefault ensures that it only happens once
+        melon_count_dict.setdefault(melon_id, {'melon_name': melon.common_name,
+                                               'melon_price': melon.price,
+                                               'total_per_type': 0,
+                                              })
 
-    print melon_count_dict
+                 
+        melon_count_dict[melon_id]['qty'] = melon_count_dict[melon_id].get('qty', 0) + 1
+        melon_count_dict[melon_id]['total_per_type'] = float(melon_count_dict[melon_id]['qty']) * melon_count_dict[melon_id]['melon_price']
+    
+    for melon_id in melon_count_dict:
+        order_total += melon_count_dict[melon_id]['total_per_type']
 
-    return render_template("cart.html",melons=melon_list)
-
+    # return render_template('cart.html', melon_dict=melon_count_dict, order_total=order_total)   
+    return render_template("cart.html", order_total=order_total)
 
 @app.route("/add_to_cart/<int:id>")
 def add_to_cart(id):
